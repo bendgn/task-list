1a6417655f264424b00da7f5ec99c87689e4ee4d
Benjie Dagunan <hello@benjiedagunan.me>
2024-08-25 02:48:31 +0800

    Title Change and Favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 11 Task List App</title>
+    <title>Simple Task List App</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <script src="//unpkg.com/alpinejs" defer></script>
 
